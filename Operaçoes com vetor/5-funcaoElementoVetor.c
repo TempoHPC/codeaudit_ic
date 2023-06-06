825829eac57dc4c39de6d6288e530b12198aeb0b
isabeldefreitas <127349333+isabeldefreitas@users.noreply.github.com>
2023-06-05 23:38:48 -0300

    mudanças relacionadas ao vetor nos parametros das funçoes

--- Operaçoes com vetor/5-funcaoElementoVetor.c
+++ Operaçoes com vetor/5-funcaoElementoVetor.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 void mostrarConteudo(int* vetNum, int numEl) {
-   
     int i;
     for (i = 0; i < numEl; i++) {
         printf("%i ", vetNum[i]);
@@ -9,21 +8,16 @@ void mostrarConteudo(int* vetNum, int numEl) {
 }
 
 int somarNumero(int* vetNum, int numEl) {
-   
-    int i, soma;
+    int i, soma = 0; 
     for (i = 0; i < numEl; i++) {
-         soma = soma + vetNum[i];
+        soma = soma + vetNum[i];
     }
     return soma;
 }
 
 int encontrarMaior(int* vetNum, int numEl) {
-    
-    int i, maior;
-
-    maior = vetNum[0];
-
-    for (i = 0; i < numEl; i++) {
+    int i, maior = vetNum[0];
+    for (i = 1; i < numEl; i++) { 
         if (vetNum[i] > maior) {
             maior = vetNum[i];
         }
@@ -32,11 +26,10 @@ int encontrarMaior(int* vetNum, int numEl) {
 }
 
 int encontrarPosicaoMaior(int* vetNum, int numEl) {
-   
-    int i, maior, posicaoMaior=0;
-    
-    for (i = 0; i < numEl; i++) {
-        if (vetNum[i] == maior) {
+    int i, maior = vetNum[0], posicaoMaior = 0;
+    for (i = 1; i < numEl; i++) { 
+        if (vetNum[i] > maior) {
+            maior = vetNum[i];
             posicaoMaior = i;
         }
     }
@@ -44,12 +37,9 @@ int encontrarPosicaoMaior(int* vetNum, int numEl) {
 }
 
 int encontrarMenor(int* vetNum, int numEl) {
-    
-    int i, menor;
-
-    menor = vetNum[0];
-    for (i = 0; i < numEl; i++) {
-        if (vetNum[i] <= menor) {
+    int i, menor = vetNum[0];
+    for (i = 1; i < numEl; i++) { 
+        if (vetNum[i] < menor) { 
             menor = vetNum[i];
         }
     }
@@ -57,11 +47,10 @@ int encontrarMenor(int* vetNum, int numEl) {
 }
 
 int encontrarPosicaoMenor(int* vetNum, int numEl) {
-    
-    int i, menor, posicaoMenor=0;
-
-    for (i = 0; i < numEl; i++) {
-        if (vetNum[i] == menor) {
+    int i, menor = vetNum[0], posicaoMenor = 0;
+    for (i = 1; i < numEl; i++) { 
+        if (vetNum[i] < menor) { 
+            menor = vetNum[i];
             posicaoMenor = i;
         }
     }
@@ -69,30 +58,37 @@ int encontrarPosicaoMenor(int* vetNum, int numEl) {
 }
 
 int main() {
-    
-    int i, numEl;
-    
+    int i, numEl=0, pos=0;
+
     printf("Digite a quantidade de elementos:\n");
     scanf("%i", &numEl);
-    
+
     int vetNum[numEl];
+
     printf("Digite os elementos:\n");
     for (i = 0; i < numEl; i++) {
-
         scanf("%i", &vetNum[i]);
     }
 
-    mostrarConteudo(&vetNum[0], 3);
+    mostrarConteudo(vetNum, numEl);
 
-    printf("\nSoma dos valores do vetor: %i\n\n", somarNumero(vetNum, numEl));
+    printf("\nDigite quantos elementos deseja mostrar:\n");
+    scanf("%i", &numEl); 
 
-    printf("Maior valor: %i\n", encontrarMaior(vetNum, numEl));
- 
-    printf("Posicao do maior: [%i]\n\n", encontrarMaior(vetNum, numEl));
+    printf("\nPartindo de qual posicao?\n");
+    scanf("%i", &pos); 
 
-    printf("Menor valor: %i\n", encontrarMenor(vetNum, numEl));
-    
-    printf("Posicao do menor: [%i]\n", encontrarPosicaoMenor(vetNum, numEl));
+    mostrarConteudo(&vetNum[pos], numEl);
+
+    printf("\nSoma dos valores do vetor: %i\n\n", somarNumero(&vetNum[pos], numEl));
+
+    printf("Maior valor: %i\n", encontrarMaior(&vetNum[pos], numEl));
+
+    printf("Posicao do maior: [%i]\n\n", encontrarPosicaoMaior(&vetNum[pos], numEl));
+
+    printf("Menor valor: %i\n", encontrarMenor(&vetNum[pos], numEl));
+
+    printf("Posicao do menor: [%i]\n", encontrarPosicaoMenor(&vetNum[pos], numEl));
 
     return 0;
 }
