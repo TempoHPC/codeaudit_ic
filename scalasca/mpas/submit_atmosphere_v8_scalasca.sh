d651d56a9b11ef36f29a8b5401b97c353977c291
rpsouto <rpsouto@lncc.br>
2024-05-23 14:08:59 -0300

    Changes to be committed: 	new file:   scorep.filt 	new file:   submit_atmosphere_v8_scalasca.sh 	new file:   submit_atmosphere_v8_scalasca_trace.sh

---
+++ scalasca/mpas/submit_atmosphere_v8_scalasca.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --nodes=1                #Número de Nós
+#SBATCH --ntasks=8               #Numero total de tarefas MPI
+#SBATCH --cpus-per-task=1        #number of cores per node
+#SBATCH -p sequana_gpu_dev       #Fila (partition) a ser utilizada
+#SBATCH -J init_atmosphere       #Nome job
+#SBATCH --time=00:20:00          #Obrigatório
+
+executable=atmosphere_model
+
+module load sequana/current
+module load git/2.23_sequana
+module load python/3.8.2_sequana
+module load cmake/3.23.2_sequana
+#module load perl/5.30_sequana
+module load openmpi/gnu/2.1.6-gcc-8.3-cuda_sequana
+. /scratch/cenapadrjsd/rpsouto/sequana/usr/local/spack/git/spack/share/spack/setup-env.sh
+export SPACK_USER_CONFIG_PATH=/scratch/cenapadrjsd/rpsouto/sequana/.spack/v0.17.1
+
+export NETCDF=$(spack location -i /5ozh6py)
+export PNETCDF=$(spack location -i /fgmjajw)
+
+spack load /swxqyxh
+
+export LD_LIBRARY_PATH=$NETCDF/lib:$PNETCDF/lib:$LD_LIBRARY_PATH
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+resultdir=scalasca/partition-${SLURM_JOB_PARTITION}/NUMNODES-$SLURM_JOB_NUM_NODES/MPI-${SLURM_NTASKS}/OMP-${SLURM_CPUS_PER_TASK}/JOBID-${SLURM_JOBID}
+
+mkdir -p ${resultdir}
+
+cd  $SLURM_SUBMIT_DIR
+
+export SCOREP_METRIC_PAPI='PAPI_TOT_INS,PAPI_TOT_CYC,PAPI_RES_STL'
+
+scalasca -analyze -s \
+mpirun -n $SLURM_NTASKS ./${executable}
+
+cp slurm-${SLURM_JOBID}.out ${resultdir}/
+mv log.atmosphere.0000.out ${resultdir}/
+mv diag.* ${resultdir}/
+
+scorepdirorig="scorep_${executable}_${SLURM_NTASKS}x${SLURM_CPUS_PER_TASK}_sum"
+mv ${scorepdirorig} ${resultdir}/
+scorepdirdest=${resultdir}/${scorepdirorig}
+
+#pós-processamento contendo análise mais detalhada
+scalasca -examine -s ${scorepdirdest}
+
+#scorep_atmosphere_model_16x1_sum
+#scorep_atmosphere_model_v8_intel_8_sum
+
+
